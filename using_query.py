d891546190a8ec7ee0e7990ca907bfb5dec7ea2e
Namsoo Kim <namsoo09.kim@samsung.com>
2021-04-16 09:59:15 +0900

    Update using_query.py
    updated ver2

--- using_query.py
+++ using_query.py
@@ -17,6 +17,15 @@ def groupby_3_10():
     m1 = abbr.get('abbr').loc[cereals.groupby('mfr').median().get('calories').sort_values(ascending=True).index[0]]
     print(m1)
 
+def groupby_3_10_ver2():
+    abbr = bpd.DataFrame().assign(
+        abbr=['A', 'G', 'K', 'N', 'P', 'Q', 'R'],
+        names=['American Home Food Products', 'General Mills', 'Kelloggs', 'Nabisco', 'Post', 'Quaker Oats', 'Ralston Purina']
+    ).set_index('abbr')
+    lowest = cereals.groupby('mfr').median().get('calories').sort_values(ascending=True).index[0]
+    little_calories = abbr.get('names').loc[cereals.groupby('mfr').median().get('calories').sort_values(ascending=True).index[0]]
+    little_calories
+
 # query_3_9()
 # print('\n\n')
 groupby_3_10()
